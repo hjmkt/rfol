f85f3d92e0e22c5e7a7c6c53659d2a9922ab4a03
hjmkt <hjmkt3126@gmail.com>
2021-01-09 01:06:12 +0900

    Add zero-ary predicate

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,8 @@ fn tokenizer_works(){
     use tokenizer::Tokenizer;
 
     let mut tokenizer = Tokenizer::new();
-    let tokens = tokenizer.tokenize("(Vx0 (Ex1 (^ (= (a x y) (b x y)) (v (~ (p y)) (q y)))))");
-    let gt = vec![LParen, Forall, Symbol("x0".into()), LParen, Exists, Symbol("x1".into()), LParen, And, LParen, Equal, LParen, Symbol("a".into()), Symbol("x".into()), Symbol("y".into()), RParen, LParen, Symbol("b".into()), Symbol("x".into()), Symbol("y".into()), RParen, RParen, LParen, Or, LParen, Not, LParen, Symbol("p".into()), Symbol("y".into()), RParen, RParen, LParen, Symbol("q".into()), Symbol("y".into()), RParen, RParen, RParen, RParen, RParen];
+    let tokens = tokenizer.tokenize("(Vx0 (Ex1 (^ (= (a x y) (b x y)) (v (~ (p y)) q))))");
+    let gt = vec![LParen, Forall, Symbol("x0".into()), LParen, Exists, Symbol("x1".into()), LParen, And, LParen, Equal, LParen, Symbol("a".into()), Symbol("x".into()), Symbol("y".into()), RParen, LParen, Symbol("b".into()), Symbol("x".into()), Symbol("y".into()), RParen, RParen, LParen, Or, LParen, Not, LParen, Symbol("p".into()), Symbol("y".into()), RParen, RParen, Symbol("q".into()),  RParen, RParen, RParen, RParen];
 
     assert_eq!(gt, tokens);
 }
@@ -24,8 +24,8 @@ fn parser_works(){
     use parser::Parser;
 
     let mut parser = Parser::new();
-    let tokens = vec![LParen, Forall, Symbol("x0".into()), LParen, Exists, Symbol("x1".into()), LParen, And, LParen, Equal, LParen, Symbol("a".into()), Symbol("x".into()), Symbol("y".into()), RParen, LParen, Symbol("b".into()), Symbol("x".into()), Symbol("y".into()), RParen, RParen, LParen, Or, LParen, Not, LParen, Symbol("p".into()), Symbol("y".into()), RParen, RParen, LParen, Symbol("q".into()), Symbol("y".into()), RParen, RParen, RParen, RParen, RParen];
-    let gt = Formula::Forall(Var("x0".into()), Box::new(Formula::Exists(Var("x1".into()), Box::new(Formula::And(Box::new(Formula::Equal(Func("a".into(), vec![Var("x".into()), Var("y".into())]), Func("b".into(), vec![Var("x".into()), Var("y".into())]))), Box::new(Formula::Or(Box::new(Formula::Not(Box::new(Formula::Pred("p".into(), vec![Var("y".into())])))), Box::new(Formula::Pred("q".into(), vec![Var("y".into())])))))))));
+    let tokens = vec![LParen, Forall, Symbol("x0".into()), LParen, Exists, Symbol("x1".into()), LParen, And, LParen, Equal, LParen, Symbol("a".into()), Symbol("x".into()), Symbol("y".into()), RParen, LParen, Symbol("b".into()), Symbol("x".into()), Symbol("y".into()), RParen, RParen, LParen, Or, LParen, Not, LParen, Symbol("p".into()), Symbol("y".into()), RParen, RParen, Symbol("q".into()), RParen, RParen, RParen, RParen];
+    let gt = Formula::Forall(Var("x0".into()), Box::new(Formula::Exists(Var("x1".into()), Box::new(Formula::And(Box::new(Formula::Equal(Func("a".into(), vec![Var("x".into()), Var("y".into())]), Func("b".into(), vec![Var("x".into()), Var("y".into())]))), Box::new(Formula::Or(Box::new(Formula::Not(Box::new(Formula::Pred("p".into(), vec![Var("y".into())])))), Box::new(Formula::Pred("q".into(), vec![])))))))));
 
     if let Ok(formula) = parser.parse(&tokens){
         assert_eq!(gt, formula);
--- src/parser.rs
+++ src/parser.rs
@@ -31,47 +31,51 @@ impl<'a> Parser<'a>{
     }
 
     fn _parse(&mut self) -> Result<Formula, String>{
-        if let Some(Token::LParen) = self.iter.next(){
-            let formula = match self.iter.next(){
-                Some(Token::Symbol(s)) => {
-                    let mut terms = vec![];
-                    loop{
-                        if let Ok(term) = self._parse_term(){ terms.push(term) }
-                        else { return Err("Parse error.".into()); }
-                        if let Some(Token::RParen) = self.iter.peek(){ break }
-                    }
-                    Ok(Formula::Pred(s.into(), terms))
-                },
-                Some(Token::Not) => {
-                    if let Ok(formula) = self._parse(){ Ok(Formula::Not(Box::new(formula))) }
-                    else{ Err("Parse error.".into()) }
-                },
-                Some(t @ Token::And | t @ Token::Or) => {
-                    if let (Ok(lhs), Ok(rhs)) = (self._parse(), self._parse()){ match t{
-                        Token::And => Ok(Formula::And(Box::new(lhs), Box::new(rhs))),
-                        _ => Ok(Formula::Or(Box::new(lhs), Box::new(rhs))),
-                    }} else { Err("Parse error.".into()) }
-                },
-                Some(Token::Equal) => {
-                    if let (Ok(lhs), Ok(rhs)) = (self._parse_term(), self._parse_term()){
-                        Ok(Formula::Equal(lhs, rhs))
-                    } else { Err("Parse error.".into()) }
-                },
-                Some(t @ Token::Forall | t @ Token::Exists) => {
-                    match (self.iter.next(), self._parse()){
-                        (Some(Token::Symbol(s)), Ok(fml)) => match t{
-                            Token::Forall => Ok(Formula::Forall(Term::Var(s.into()), Box::new(fml))),
-                            Token::Exists => Ok(Formula::Exists(Term::Var(s.into()), Box::new(fml))),
-                            _ => Err("Parse error.".into())
-                        },
-                        _ => Err("Parse error.".into()),
-                    }
-                },
-                _ => Err("Parse error.".into()),
-            };
-            if let Some(Token::RParen) = self.iter.next() { formula }
-            else { Err("Parse error.".into()) }
-        } else{ Err("Parse error.".into()) }
+        match self.iter.next(){
+            Some(Token::LParen) => {
+                let formula = match self.iter.next(){
+                    Some(Token::Symbol(s)) => {
+                        let mut terms = vec![];
+                        loop{
+                            if let Ok(term) = self._parse_term(){ terms.push(term) }
+                            else { return Err("Parse error.".into()); }
+                            if let Some(Token::RParen) = self.iter.peek(){ break }
+                        }
+                        Ok(Formula::Pred(s.into(), terms))
+                    },
+                    Some(Token::Not) => {
+                        if let Ok(formula) = self._parse(){ Ok(Formula::Not(Box::new(formula))) }
+                        else{ Err("Parse error.".into()) }
+                    },
+                    Some(t @ Token::And | t @ Token::Or) => {
+                        if let (Ok(lhs), Ok(rhs)) = (self._parse(), self._parse()){ match t{
+                            Token::And => Ok(Formula::And(Box::new(lhs), Box::new(rhs))),
+                            _ => Ok(Formula::Or(Box::new(lhs), Box::new(rhs))),
+                        }} else { Err("Parse error.".into()) }
+                    },
+                    Some(Token::Equal) => {
+                        if let (Ok(lhs), Ok(rhs)) = (self._parse_term(), self._parse_term()){
+                            Ok(Formula::Equal(lhs, rhs))
+                        } else { Err("Parse error.".into()) }
+                    },
+                    Some(t @ Token::Forall | t @ Token::Exists) => {
+                        match (self.iter.next(), self._parse()){
+                            (Some(Token::Symbol(s)), Ok(fml)) => match t{
+                                Token::Forall => Ok(Formula::Forall(Term::Var(s.into()), Box::new(fml))),
+                                Token::Exists => Ok(Formula::Exists(Term::Var(s.into()), Box::new(fml))),
+                                _ => Err("Parse error.".into())
+                            },
+                            _ => Err("Parse error.".into()),
+                        }
+                    },
+                    _ => Err("Parse error.".into()),
+                };
+                if let Some(Token::RParen) = self.iter.next() { formula }
+                else { Err("Parse error.".into()) }
+            },
+            Some(Token::Symbol(s)) => Ok(Formula::Pred(s.into(), vec![])),
+            _ => Err("Parse error.".into())
+        }
     }
 
     pub fn parse(&mut self, tokens: &'a Vec<Token>) -> Result<Formula, String>{
@@ -79,3 +83,4 @@ impl<'a> Parser<'a>{
         self._parse()
     }
 }
+
