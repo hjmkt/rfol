f02ff51f947aae68a46978cb64de536664722892
hjmkt <hjmkt3126@gmail.com>
2021-01-10 03:28:56 +0900

    Add finite model and formula evaluator

--- src/data.rs
+++ src/data.rs
@@ -17,7 +17,7 @@ pub enum Token {
     Exists,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum Formula {
     Pred(String, Vec<Term>),
     Equal(Term, Term),
@@ -175,5 +175,87 @@ impl Formula {
         let mut preds = HashSet::new();
         self._get_preds(&mut preds);
         preds
+    }
+}
+
+use std::collections::HashMap;
+
+pub struct FiniteModel {
+    pub domain_size: u32,
+    pub var_assignment: HashMap<Term, u32>,
+    pub func_assignment: HashMap<NonLogicalSymbol, HashMap<Vec<u32>, u32>>,
+    pub pred_assignment: HashMap<NonLogicalSymbol, HashMap<Vec<u32>, bool>>,
+}
+
+impl FiniteModel{
+    pub fn new(domain_size: u32) -> FiniteModel{
+        FiniteModel{
+            domain_size: domain_size,
+            var_assignment: HashMap::new(),
+            func_assignment: HashMap::new(),
+            pred_assignment: HashMap::new(),
+        }
+    }
+}
+
+trait Model {
+    fn evaluate_term(&self, term: &Term) -> u32;
+    fn evaluate_formula(&mut self, fml: &Formula) -> bool;
+}
+
+impl Model for FiniteModel {
+    fn evaluate_term(&self, term: &Term) -> u32 {
+        match term {
+            var @ Term::Var(_) => self.var_assignment[&var],
+            Term::Func(name, terms) => {
+                let func = NonLogicalSymbol {
+                    name: name.into(),
+                    arity: terms.len() as u32,
+                };
+                let assignment = &self.func_assignment[&func];
+                let values = terms
+                    .iter()
+                    .map(|term| self.evaluate_term(term))
+                    .collect::<Vec<u32>>();
+                assignment[&values]
+            }
+        }
+    }
+
+    fn evaluate_formula(&mut self, fml: &Formula) -> bool {
+        match fml {
+            Formula::Pred(name, terms) => {
+                let pred = NonLogicalSymbol {
+                    name: name.into(),
+                    arity: terms.len() as u32,
+                };
+                let assignment = &self.pred_assignment[&pred];
+                let values = terms
+                    .iter()
+                    .map(|term| self.evaluate_term(term))
+                    .collect::<Vec<u32>>();
+                assignment[&values]
+            }
+            Formula::Equal(lhs, rhs) => self.evaluate_term(lhs) == self.evaluate_term(rhs),
+            Formula::Not(bfml) => self.evaluate_formula(bfml),
+            Formula::And(lhs, rhs) => self.evaluate_formula(lhs) && self.evaluate_formula(rhs),
+            Formula::Or(lhs, rhs) => self.evaluate_formula(lhs) || self.evaluate_formula(rhs),
+            Formula::Forall(Term::Var(name), bfml) => (0..self.domain_size)
+                .map(|v| {
+                    self.var_assignment.insert(Term::Var(name.into()), v);
+                    self.evaluate_formula(bfml)
+                })
+                .any(std::convert::identity),
+            Formula::Exists(Term::Var(name), bfml) => !(0..self.domain_size)
+                .map(|v| {
+                    self.var_assignment.insert(Term::Var(name.into()), v);
+                    !self.evaluate_formula(bfml)
+                })
+                .any(std::convert::identity),
+            _ => {
+                assert!(false);
+                false
+            }
+        }
     }
 }
